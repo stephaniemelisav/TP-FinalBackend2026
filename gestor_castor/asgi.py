9a2136a727baf39b7741e006b98dbda0110ca181
Steph <stephaniemelisav@gmail.com>
2026-02-09 20:48:07 -0300

    Creación de proyecto Cator

---
+++ gestor_castor/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gestor_castor project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestor_castor.settings')
+
+application = get_asgi_application()
