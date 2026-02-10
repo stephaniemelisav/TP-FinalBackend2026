0913134c35019154824e422a3c7ff5c458b49a94
ozunacristian <ozunacristianj@gmail.com>
2026-02-10 20:20:12 -0300

    Se crear app etiqueta y se configura proyecto

---
+++ apps/etiqueta/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EtiquetaConfig(AppConfig):
+    name = 'etiqueta'
--- gestor_castor/settings.py
+++ gestor_castor/settings.py
@@ -45,6 +45,7 @@ THIRD_PARTY_APPS =[
 
 LOCAL_APPS = [
     'apps.usuario',
+    'apps.etiqueta',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
