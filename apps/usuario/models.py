2bf52f5d241eeae9b559cd606276b05fcd57a903
ozunacristian <ozunacristianj@gmail.com>
2026-02-10 20:41:56 -0300

    se modifica modelo de aplicacion usuario.

--- apps/usuario/models.py
+++ apps/usuario/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Se hereda abstractuser por si en un futuro se requiere ampliar los atributos de un usuario.
+class Usuario(AbstractUser):
+    
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+    
+    def __str__(self):
+        return self.username
