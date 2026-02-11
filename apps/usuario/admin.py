f2f4a4f30470665f055dd68e5eae73af98682fce
ozunacristian <ozunacristianj@gmail.com>
2026-02-10 22:18:22 -0300

    Se registra aplicacion usuario en el panel de admin.

--- apps/usuario/admin.py
+++ apps/usuario/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from apps.usuario.models import Usuario
 
-# Register your models here.
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    model = Usuario
+    list_display = ("username", "email", "first_name", "last_name", "is_staff", "is_superuser", "is_active",)
+    list_filter = ("is_staff", "is_superuser", "is_active", "groups")
+    search_fields = ("username", "email", "first_name", "last_name")
